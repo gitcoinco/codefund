7fc32ba436d386b13b5f87fa8f60c007465ba803
Eric Berry <cavneb@gmail.com>
2018-03-09 09:19:42 -0700

    Configure for Heroku deployment

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: MIX_ENV=prod mix phx.server
+release: POOL_SIZE=2 mix ecto.migrate
--- lib/code_sponsor_web/channels/user_socket.ex
+++ lib/code_sponsor_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule CodeSponsorWeb.UserSocket do
   # channel "room:*", CodeSponsorWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
