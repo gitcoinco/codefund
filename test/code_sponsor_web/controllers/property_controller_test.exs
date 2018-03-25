3db4fa1e53a4da8d81b36d275804838b06674977
Zacck Osiemo <coderv63@gmail.com>
2018-03-25 14:32:17 +0200

    Add property controller action tests

--- test/code_sponsor_web/controllers/property_controller_test.exs
+++ test/code_sponsor_web/controllers/property_controller_test.exs
@@ -26,5 +26,73 @@ defmodule CodeSponsorWeb.PropertyControllerTest do
       assert html_response(conn, 200)
     end
 
+    test "show should show a property", %{conn: conn} do
+      user = insert(:user, %{roles: ["developer"]})
+      conn = assign conn, :current_user, user
+      property = insert(:property)
+      conn = get conn, property_path(conn, :show, property)
+      assert html_response(conn, 200) =~ "Edit Property"
+    end
+
+    test "edit should show a property edit form", %{conn: conn} do
+      user = insert(:user, %{roles: ["developer"]})
+      conn = assign conn, :current_user, user
+      property = insert(:property)
+      conn = get conn, property_path(conn, :edit, property)
+      assert html_response(conn, 200) =~ "Edit Property"
+    end
+
+    test "deletes a property", %{conn: conn} do
+      user = insert(:user, %{roles: ["developer"]})
+      conn = assign conn, :current_user, user
+      property = insert(:property)
+      assert Repo.aggregate(CodeSponsor.Schema.Property, :count, :id) == 1
+      conn = delete conn, property_path(conn, :delete, property)
+      assert html_response(conn, 302)
+      assert Repo.aggregate(CodeSponsor.Schema.Property, :count, :id) == 0
+
+    end
+
+    # test "create/2", %{conn: conn} do
+    #   user = insert(:user, %{roles: ["admin"]})
+    #   conn = assign conn, :current_user, user
+    #   assert Repo.aggregate(CodeSponsor.Schema.Property, :count, :id) == 0
+    #   params = %{
+    #     "property" => %{
+    #       "name" => "property_name",
+    #       "url" => "https://logivo.b",
+    #       "property_type" => 1,
+    #       "user_id" => user.id
+    #     }
+    #   }
+    #   conn = post conn, property_path(conn, :create), params
+    #   assert html_response(conn, 302)
+    #   assert Repo.aggregate(CodeSponsor.Schema.Property, :count, :id) == 1
+    # end
+
+    # test "update/2", %{conn: conn} do
+    #   user = insert(:user, %{roles: ["admin"]})
+    #   conn = assign conn, :current_user, user
+    #   property = insert(:property)
+    #   assert Repo.aggregate(CodeSponsor.Schema.Property, :count, :id) == 1
+    #   params = %{
+    #     "property" => %{
+    #       "name" => "property_name",
+    #       "url" => "https://logivo.b",
+    #       "property_type" => 1,
+    #       "user_id" => user.id
+    #     }
+    #   }
+    #   conn = patch conn, property_path(conn, :update, property), params
+    #   assert html_response(conn, 302)
+    #   updated_prop = Repo.one(CodeSponsor.Schema.Property)
+    #   assert updated_prop.name == params["property"]["name"]
+    # end
+
+
+
+
+
+
   end
 end
