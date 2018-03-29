0f2f54f68a16d61bc4cf9dfe139cb463eea65837
Justin Bean <justin.r.bean@gmail.com>
2018-03-29 13:31:17 -0600

    bump elixir version and add formatter config

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CodeFund.Mixfile do
     [
       app: :code_fund,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.6.2",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
