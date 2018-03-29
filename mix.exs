412ee5471e154270cb81949433b49f3c5c893c64
Eric Berry <cavneb@gmail.com>
2018-03-29 14:32:34 -0600

    Bumped elixir to 1.6.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CodeFund.Mixfile do
     [
       app: :code_fund,
       version: "0.0.1",
-      elixir: "~> 1.6.2",
+      elixir: "~> 1.6.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
