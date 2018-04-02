83af4c002382bc97fa4cb002404258e8b471f828
karolin siebert <karolin@intellisense.io>
2018-04-02 19:54:34 +0200

    import css from node_modules

--- assets/src/controllers/dashboard_controller.js
+++ assets/src/controllers/dashboard_controller.js
@@ -2,10 +2,16 @@ import { Controller } from "stimulus";
 import _ from "lodash";
 import moment from "moment";
 import Chart from "chart.js";
+import daterangepicker from "bootstrap-daterangepicker";
+
+import $ from "jquery/dist/jquery";
+/* 
+import jQuery from "detached-jquery-1.10.2";
+const $ = jQuery.getJQuery(); */
 
 export default class extends Controller {
   static get targets() {
-    return ["trafficImpressionsChart", "trafficClicksChart"];
+    return ["trafficImpressionsChart", "trafficClicksChart", "dateRange"];
   }
 
   connect() {
@@ -15,7 +21,32 @@ export default class extends Controller {
     this.loadTrafficImpressionsChart(impressionsByDay);
     this.loadTrafficClicksChart(clicksByDay);
 
+    this.initDatePicker();
+
     console.log("Loaded dashboard");
+  }
+
+  initDatePicker() {
+    const startDate = this.element.dataset.startDate;
+    const endDate = this.element.dataset.endDate;
+
+    const picker = $(this.dateRangeTarget);
+    picker.daterangepicker(
+      {
+        startDate: moment(startDate),
+        endDate: moment(endDate)
+      },
+      (start, end) => {
+        console.log(start, end);
+      }
+    );
+
+    console.log(startDate);
+    console.log(endDate);
+
+    picker.on("apply.daterangepicker", (ev, picker) => {
+      console.log("APPLYING");
+    });
   }
 
   strToDate(str) {
