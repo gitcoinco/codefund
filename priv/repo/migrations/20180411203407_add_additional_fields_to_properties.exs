938b962dda14b10dbaa5556d87354930da03472a
Justin Bean <justin.r.bean@gmail.com>
2018-04-12 17:22:09 -0600

    only show certain form fields if admin on properties page

--- priv/repo/migrations/20180411203407_add_additional_fields_to_properties.exs
+++ priv/repo/migrations/20180411203407_add_additional_fields_to_properties.exs
@@ -7,8 +7,8 @@ defmodule CodeFund.Repo.Migrations.AddAdditionalFieldsToProperties do
       add :estimated_monthly_visitors, :integer
       add :alexa_site_rank, :integer
       add :language, :string
-      add :programming_languages, {:array, :string}
-      add :topic_categories, {:array, :string}
+      add :programming_languages, {:array, :string}, default: "{}"
+      add :topic_categories, {:array, :string}, default: "{}"
       add :screenshot_url, :text
     end
   end
