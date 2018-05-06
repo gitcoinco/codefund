e3aaafa7c88611cfe0a08a43e9e4f7efe4c1d9ad
Justin Bean <justin.r.bean@gmail.com>
2018-05-06 16:48:31 -0600

    bean/cleanup

--- lib/code_fund_web/controllers/theme_controller.ex
+++ lib/code_fund_web/controllers/theme_controller.ex
@@ -30,7 +30,7 @@ defmodule CodeFundWeb.ThemeController do
     assigns(template_choices: controller_assigns())
   end
 
-  def controller_assigns() do
+  defp controller_assigns() do
     CodeFund.Templates.list_templates() |> FormHelpers.repo_objects_to_options()
   end
 end
