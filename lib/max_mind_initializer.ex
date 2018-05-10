224a4dbb0119c98b548b4e9cfc29a756c65f03bf
Justin Bean <justin.r.bean@gmail.com>
2018-05-10 17:35:24 -0600

    get correct heroku priv path

--- lib/max_mind_initializer.ex
+++ lib/max_mind_initializer.ex
@@ -3,18 +3,21 @@ defmodule MaxMindInitializer do
   def init(database) do
     maxmind_db = Application.get_env(:geolix, :mmdb_location) |> HTTPoison.get!()
 
-    File.write(Path.expand("../priv/maxmind/GeoIP2-Country.mmdb", __DIR__), maxmind_db.body)
+    File.write(get_priv_dir(Mix.env), maxmind_db.body)
 
     databases = [
       %{
         id: :country,
         adapter: Geolix.Adapter.MMDB2,
-        source: Path.expand("../priv/maxmind/GeoIP2-Country.mmdb", __DIR__)
+        source: get_priv_dir(Mix.env)
       }
       | Application.get_env(:geolix, :databases, [])
     ]
 
     Application.put_env(:geolix, :databases, databases)
-    Map.put(database, :source, Path.expand("../priv/maxmind/GeoIP2-Country.mmdb", __DIR__))
+    Map.put(database, :source, get_priv_dir(Mix.env))
   end
+
+  defp get_priv_dir(:prod), do: "/app/_build/prod/lib/code_fund/priv/maxmind"
+  defp get_priv_dir(_), do: Path.expand("../priv/maxmind/GeoIP2-Country.mmdb", __DIR__)
 end
