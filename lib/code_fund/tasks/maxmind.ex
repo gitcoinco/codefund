c0b2aa69432ae6e498d5be63a9a9d1c795979ebf
Justin Bean <justin.r.bean@gmail.com>
2018-05-15 10:10:06 -0600

    heroku maxmind fix

--- lib/code_fund/tasks/maxmind.ex
+++ lib/code_fund/tasks/maxmind.ex
@@ -7,14 +7,17 @@ defmodule Mix.Tasks.Maxmind.Setup do
       System.get_env("MMDB_LOCATION") ||
         "http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz"
 
+    tmp_dir = System.tmp_dir()
+    location_of_tarball = tmp_dir <> "/mmdb.tar.gz"
+
     "curl"
     |> System.cmd([
       "-o",
-      "mmdb.tar.gz",
+      location_of_tarball,
       maxmind_url
     ])
 
-    files = "tar" |> System.cmd(["-tvf", "mmdb.tar.gz"])
+    files = "tar" |> System.cmd(["-tvf", location_of_tarball])
 
     file_name =
       files
@@ -25,9 +28,11 @@ defmodule Mix.Tasks.Maxmind.Setup do
       |> String.split(" ")
       |> List.last()
 
-    "tar" |> System.cmd(["-xvzf", "mmdb.tar.gz", file_name])
-    "mv" |> System.cmd([file_name, "./priv/maxmind/GeoIP2-Country.mmdb"])
-    "rm" |> System.cmd(["mmdb.tar.gz"])
-    "rm" |> System.cmd(["-rf", file_name |> String.split("/") |> List.first()])
+    "tar" |> System.cmd(["-xvzf", location_of_tarball, "-C", tmp_dir, file_name])
+    "mv" |> System.cmd([tmp_dir <> "/" <> file_name, MaxMindInitializer.get_priv_dir(Mix.env())])
+    "rm" |> System.cmd([location_of_tarball])
+
+    "rm"
+    |> System.cmd(["-rf", tmp_dir <> "/" <> (file_name |> String.split("/") |> List.first())])
   end
 end
--- lib/max_mind_initializer.ex
+++ lib/max_mind_initializer.ex
@@ -17,8 +17,9 @@ defmodule MaxMindInitializer do
     Map.put(database, :source, get_priv_dir(Mix.env()))
   end
 
-  defp get_priv_dir(:prod), do: "/tmp/GeoIP2-Country.mmdb"
-  defp get_priv_dir(_), do: Path.expand("../priv/maxmind/GeoIP2-Country.mmdb", __DIR__)
+  def get_priv_dir(:prod), do: "/tmp/GeoIP2-Country.mmdb"
+
+  def get_priv_dir(_env), do: Path.expand("../priv/maxmind/GeoIP2-Country.mmdb", __DIR__)
 
   defp write_max_mind_db({:error, :enoent}) do
     case Application.get_env(:geolix, :mmdb_location) do
