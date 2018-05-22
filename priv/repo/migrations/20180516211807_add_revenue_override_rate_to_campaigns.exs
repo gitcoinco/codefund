6c2dd69b94a8ac59094d4e0319831d41ebecc04f
Justin Bean <justin.r.bean@gmail.com>
2018-05-22 11:40:43 -0600

    fix migration issue with campaign override revenue

--- priv/repo/migrations/20180516211807_add_revenue_override_rate_to_campaigns.exs
+++ priv/repo/migrations/20180516211807_add_revenue_override_rate_to_campaigns.exs
@@ -3,7 +3,7 @@ defmodule CodeFund.Repo.Migrations.AddRevenueOverrideRateToCampaigns do
 
   def change do
     alter table(:campaigns) do
-      add :override_revenue_rate, :decimal, precision: 3, scale: 3, default: 0
+      add :override_revenue_rate, :decimal, precision: 3, scale: 3
     end
   end
 end
