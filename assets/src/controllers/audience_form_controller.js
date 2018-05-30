2a9cd0ee6c716f9a022661698e67452ef129ed99
Eric Berry <cavneb@gmail.com>
2018-05-30 14:35:35 -0600

    WIP

--- assets/src/controllers/audience_form_controller.js
+++ assets/src/controllers/audience_form_controller.js
@@ -1,45 +1,84 @@
-import { Controller } from "stimulus";
-import axios from "../utils/axios";
-import $ from "jquery/dist/jquery";
-import _ from "lodash";
+// import { Controller } from "stimulus";
+// import axios from "../utils/axios";
+// import qs from "qs";
+// import $ from "jquery/dist/jquery";
+// import _ from "lodash";
 
-export default class extends Controller {
-  static get targets() {
-    return [
-      "name",
-      "programmingLanguages",
-      "topicCategories",
-      "excludedCountries"
-    ];
-  }
+// export default class extends Controller {
+//   static get targets() {
+//     return [
+//       "name",
+//       "programmingLanguages",
+//       "topicCategories",
+//       "excludedCountries",
+//       "uniqueUserCount",
+//       "propertyCount",
+//       "impressionCount"
+//     ];
+//   }
 
-  connect() {
-    this.payload = {};
-    this.initSelectize();
-    console.log("HELLO!");
-  }
+//   connect() {
+//     this.payload = {
+//       filters: {
+//         programming_languages: [],
+//         topic_categories: [],
+//         excluded_countries: []
+//       }
+//     };
+//     this.initSelectize();
+//   }
 
-  initSelectize() {
-    YOU ARE HERE!
-    $("select.selectize").selectize({
-      plugins: ["remove_button"],
-      delimiter: ",",
-      persist: false
-    });
-  }
+//   initSelectize() {
+//     $("select.audience-form-selectize")
+//       .selectize({
+//         plugins: ["remove_button"],
+//         delimiter: ",",
+//         persist: false
+//       })
+//       .on("change", this.generateEstimates.bind(this));
+//   }
 
-  generateEstimates(event) {
-    var change = {};
-    change[event.currentTarget.dataset.key] = event.currentTarget.value;
-    this.payload = Object.assign(this.payload, change);
-  }
+//   generateEstimates(event) {
+//     let payload = this.payload;
+    
+//     console.log(payload);
 
-  get payload() {
-    return JSON.parse(this.data.get("payload"));
-  }
+//     if (event.currentTarget.attributes.class.value.includes("audience-form-selectize")) {
+//       const options = _.map(event.currentTarget.selectedOptions, (opt) => {
+//         return opt.value;
+//       });
+//       payload['filters'][event.currentTarget.dataset.key] = options;
+//     } else {
+//       payload['filters'][event.currentTarget.dataset.key] = event.currentTarget.value;
+//     }
+//     this.payload = payload;
+//     const query = qs.stringify(this.payload, { arrayFormat: "brackets" });
+//     axios({
+//       url: `/audience_metrics?${query}`,
+//       method: 'GET',
+//       headers: {
+//         'content-type': 'application/x-www-form-urlencoded'
+//       }
+//     }).then(this.handleMetricsResponse.bind(this));
+//   }
 
-  set payload(value) {
-    this.data.set("payload", JSON.stringify(value));
-    console.log(JSON.stringify(value));
-  }
-}
+//   handleMetricsResponse(response) {
+//     if (response.status === 200) {
+//       this.uniqueUserCountTarget.innerText = response.data.unique_user_count;
+//       this.propertyCountTarget.innerText = response.data.property_count;
+//       this.impressionCountTarget.innerText = response.data.impression_count;
+//     } else {
+//       this.uniqueUserCountTarget.innerText = "-";
+//       this.propertyCountTarget.innerText = "-";
+//       this.impressionCountTarget.innerText = "-";
+//     }
+//   }
+
+//   get payload() {
+//     return JSON.parse(this.data.get("payload"));
+//   }
+
+//   set payload(value) {
+//     this.data.set("payload", JSON.stringify(value));
+//   }
+// }
