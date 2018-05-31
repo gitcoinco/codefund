4ed7605af4f9afcce8764d359bf1d17ab81c4f79
Justin Bean <justin.r.bean@gmail.com>
2018-05-31 07:20:14 -0600

    add_excluded_countries_into_audience_metrics (#178)

--- lib/ad_service/query/for_audience_creation.ex
+++ lib/ad_service/query/for_audience_creation.ex
@@ -2,13 +2,16 @@ defmodule AdService.Query.ForAudienceCreation do
   import Ecto.Query
 
   def metrics(filters) do
-    filters = Keyword.delete(filters, :excluded_countries)
+    {excluded_countries, filters} =
+      filters
+      |> split_countries_and_filters
 
     from(
       impression in CodeFund.Schema.Impression,
       where:
         impression.inserted_at >= ^beginning_of_last_month() and
           impression.inserted_at <= ^end_of_last_month(),
+      where: impression.country not in ^excluded_countries,
       join: property in assoc(impression, :property),
       select: %{
         "impression_count" => count(impression.id),
@@ -18,6 +21,13 @@ defmodule AdService.Query.ForAudienceCreation do
     )
     |> AdService.Query.Shared.build_where_clauses_by_property_filters(filters)
     |> CodeFund.Repo.one()
+  end
+
+  defp split_countries_and_filters(filters) do
+    case Keyword.pop(filters, :excluded_countries) do
+      {nil, filters} -> {[], filters}
+      {excluded_countries, filters} -> {excluded_countries, filters}
+    end
   end
 
   defp beginning_of_last_month() do
