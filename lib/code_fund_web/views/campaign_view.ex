4ad993396c083b70444de5b77eab6c1bbf8b042f
Justin Bean <justin.r.bean@gmail.com>
2018-06-18 13:15:03 -0600

    bean/ch-135_campaign_page_timeout

--- lib/code_fund_web/views/campaign_view.ex
+++ lib/code_fund_web/views/campaign_view.ex
@@ -2,13 +2,4 @@ defmodule CodeFundWeb.CampaignView do
   use CodeFundWeb, :view
 
   import CodeFundWeb.TableView
-  alias CodeFund.Schema.Campaign
-  alias CodeFund.Campaigns
-
-  def has_remaining_budget?(%Campaign{} = campaign) do
-    case Campaigns.has_remaining_budget?(campaign) do
-      true -> content_tag(:span, "Yes", class: "badge badge-success")
-      false -> content_tag(:span, "No", class: "badge badge-light")
-    end
-  end
 end
