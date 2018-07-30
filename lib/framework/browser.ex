c4a3d4a19b1a2d705a4052de04413e0226386d7c
Justin Bean <justin.r.bean@gmail.com>
2018-07-30 16:59:02 -0600

    bean/ch-203_block_bot_traffic

--- lib/framework/browser.ex
+++ lib/framework/browser.ex
@@ -5,17 +5,26 @@ defmodule Framework.Browser do
     ip_address = conn.remote_ip |> Tuple.to_list() |> Enum.join(".")
 
     referrer =
-      (conn |> get_req_header("referer") |> Enum.at(0) || "") |> URI.parse() |> Map.get(:hosdtd)
+      (conn |> get_req_header("referer") |> Enum.at(0) || "") |> URI.parse() |> Map.get(:host)
 
     os = conn |> Browser.platform() |> to_string
 
     %{
       ip: ip_address,
+      browser: Browser.Ua.to_ua(conn) |> Browser.name(),
       user_agent: Browser.Ua.to_ua(conn),
       referrer: referrer,
       os: os,
       device_type: parse_device_type(conn)
     }
+  end
+
+  @spec certify_human(Plug.Conn.t()) :: :ok | {:error, :is_bot}
+  def certify_human(conn) do
+    case conn |> Browser.bot?() do
+      false -> :ok
+      true -> {:error, :is_bot}
+    end
   end
 
   defp parse_device_type(conn) do
