4116901e0b6d2be76af836f3927b67090a610afc
Justin Bean <justin.r.bean@gmail.com>
2018-08-24 10:25:23 -0600

    bean/ch-272_advertiser_publisher_dashboard (#278)

--- lib/code_fund_web/controllers/dashboard_controller.ex
+++ lib/code_fund_web/controllers/dashboard_controller.ex
@@ -2,15 +2,28 @@ defmodule CodeFundWeb.DashboardController do
   use CodeFundWeb, :controller
   import Joken
 
+  @spec index(Plug.Conn.t(), any()) :: Plug.Conn.t()
   def index(conn, _params) do
-    # Build URL for Metabase
+    fn ->
+      conn.assigns.current_user
+      |> Map.put(:roles, conn.assigns.current_user.roles |> Enum.sort())
+      |> Metabase.Helpers.dashboard_map()
+    end
+    |> metabase_render(conn)
+  end
+
+  @spec publisher_dashboard(Plug.Conn.t(), any()) :: Plug.Conn.t()
+  def publisher_dashboard(conn, _params) do
+    fn -> Metabase.Helpers.developer_dashboard_map(conn.assigns.current_user) end
+    |> metabase_render(conn)
+  end
+
+  defp metabase_render(map_function, conn) do
     metabase_site_url = Application.get_env(:code_fund, CodeFundWeb.Endpoint)[:metabase_site_url]
     secret_key = Application.get_env(:code_fund, CodeFundWeb.Endpoint)[:metabase_secret_key]
 
     metabase_token =
-      conn.assigns.current_user
-      |> Map.put(:roles, conn.assigns.current_user.roles |> Enum.sort())
-      |> Metabase.Helpers.dashboard_map()
+      map_function.()
       |> token
       |> with_signer(hs256(secret_key))
       |> sign
--- lib/metabase/helpers.ex
+++ lib/metabase/helpers.ex
@@ -4,11 +4,24 @@ defmodule Metabase.Helpers do
   def dashboard_map(%CodeFund.Schema.User{roles: ["admin" | _roles]}),
     do: %{resource: %{dashboard: dashboard_role_mapping(:admin)}, params: %{}}
 
-  def dashboard_map(%CodeFund.Schema.User{id: id, roles: ["sponsor" | _roles]}),
-    do: %{resource: %{dashboard: dashboard_role_mapping(:sponsor)}, params: %{user_id: id}}
+  def dashboard_map(%CodeFund.Schema.User{id: id, roles: [_roles | ["sponsor"]]}),
+    do: advertiser_dashboard_map(id)
 
-  def dashboard_map(%CodeFund.Schema.User{id: id}),
+  def dashboard_map(%CodeFund.Schema.User{id: id, roles: ["sponsor"]}),
+    do: advertiser_dashboard_map(id)
+
+  def dashboard_map(%CodeFund.Schema.User{} = user),
+    do: user |> developer_dashboard_map
+
+  @spec developer_dashboard_map(CodeFund.Schema.User.t()) :: %{
+          params: %{user_id: any()},
+          resource: %{dashboard: integer()}
+        }
+  def developer_dashboard_map(%CodeFund.Schema.User{id: id}),
     do: %{resource: %{dashboard: dashboard_role_mapping(:user)}, params: %{user_id: id}}
+
+  defp advertiser_dashboard_map(user_id),
+    do: %{resource: %{dashboard: dashboard_role_mapping(:sponsor)}, params: %{user_id: user_id}}
 
   @spec dashboard_role_mapping(atom) :: integer
   defp dashboard_role_mapping(role),
