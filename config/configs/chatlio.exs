1bb3b818ca46b4ba93c606cdc1f310732e6ee7aa
Eric Berry <eric@berry.sh>
2018-09-05 08:58:26 -0600

    Replace Chatlio with Groove (#295)
    [ch333]

--- config/configs/chatlio.exs
+++
@@ -1,3 +0,0 @@
-use Mix.Config
-
-config :code_fund, CodeFundWeb.Endpoint, chatlio_widget_id: System.get_env("CHATLIO_WIDGET_ID")
---
+++ config/configs/support_widget.exs
@@ -0,0 +1,3 @@
+use Mix.Config
+
+config :code_fund, CodeFundWeb.Endpoint, support_widget_id: System.get_env("SUPPORT_WIDGET_ID")
