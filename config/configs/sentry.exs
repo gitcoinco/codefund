af5800eb810a5a8a4364253d29008c7d6440031a
Justin Bean <justin.r.bean@gmail.com>
2018-09-21 15:15:45 -0600

    try to get more verbose logs from sentry

--- config/configs/sentry.exs
+++ config/configs/sentry.exs
@@ -3,4 +3,5 @@ use Mix.Config
 config :sentry,
   dsn: System.get_env("SENTRY_DSN") || "",
   included_environments: [:prod],
-  environment_name: Mix.env()
+  environment_name: Mix.env(),
+  context_lines: 25
