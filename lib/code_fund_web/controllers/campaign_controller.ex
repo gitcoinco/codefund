5473cdb8ae02d22f392608a8eef25f02c9843683
Nathan Hopkins <natehop@gmail.com>
2018-10-01 18:20:16 -0600

    Work on campaign form to show/hide enable/disable fields based on role

--- lib/code_fund_web/controllers/campaign_controller.ex
+++ lib/code_fund_web/controllers/campaign_controller.ex
@@ -67,129 +67,158 @@ defmodule CodeFundWeb.CampaignController do
     |> form_fields(campaign.user)
   end
 
-  defp form_fields(true, user) do
+  defp form_fields(is_admin, user) do
     [audiences: audiences, creatives: creatives] = audiences_and_creatives_by_user(user)
 
-    [
-      fields: [
-        name: [type: :text_input, label: "Name"],
-        redirect_url: [type: :text_input, label: "Redirect URL", opts: [placeholder: "https://"]],
-        status: [
-          type: :select,
-          label: "Status",
-          opts: [choices: CodeFund.Campaigns.statuses()]
-        ],
-        active_dates: [
-          type: :text_input,
-          label: "Date Range",
-          opts: [
-            "data-target": "date-range.datePicker"
+    fields = [
+      user_id: [
+        type: :select,
+        label: "Owner",
+        opts: [
+          disabled: !is_admin,
+          prompt: "Select a User",
+          choices:
+            CodeFund.Users.get_by_role("sponsor")
+            |> FormHelpers.repo_objects_to_options([:first_name, :last_name], " "),
+          data: [
+            target: "campaign-form.userId",
+            action: "change->campaign-form#creativesForUser"
           ]
-        ],
-        creative_id: [
-          type: :select,
-          label: "Creative",
-          opts: [
-            prompt: "Select a Creative",
-            choices: creatives,
-            "data-target": "campaign-form.creatives"
-          ]
-        ],
-        audience_id: [type: :select, label: "Audience", opts: [choices: audiences]],
-        user_id: [
-          type: :select,
-          label: "User",
-          opts: [
-            prompt: "Select a User",
-            choices:
-              CodeFund.Users.get_by_role("sponsor")
-              |> FormHelpers.repo_objects_to_options([:first_name, :last_name], " "),
-            "data-target": "campaign-form.userId",
-            "data-action": "change->campaign-form#creativesForUser"
-          ]
-        ],
-        ecpm: [
-          type: :currency_input,
-          label: "eCPM",
-          opts: [
-            step: "0.01",
-            min: "0",
-            "data-target": "campaign-form.ecpm",
-            "data-action": "change->campaign-form#calculateImpressions",
-            step: "0.01"
-          ]
-        ],
-        included_countries: [
-          type: :multiple_select,
-          label: "Included Countries",
-          opts: [
-            data: [
-              target: "campaign-form.includedCountries",
-              action: "change->campaign-form#generateEstimates",
-              key: "included_countries"
-            ],
-            class: "form-control campaign-form selectize",
-            choices: Framework.Geolocation.Countries.list(),
-            hint: ~s"""
-            <div>
-              <span class="click-option" data-action='click->campaign-form#selectRecommendedCountries'>Recommended</span> |
-              <span class="click-option" data-action='click->campaign-form#selectPopularCountries'>Popular</span> |
-              <span class="click-option" data-action='click->campaign-form#selectAllCountries'>All</span> |
-              <span class="click-option" data-action='click->campaign-form#selectNoCountries'>None</span>
-            </div>
-            """
-          ]
-        ],
-        budget_daily_amount: [
-          type: :currency_input,
-          label: "Daily Max Spend",
-          opts: [step: "0.01"]
-        ],
-        total_spend: [
-          type: :currency_input,
-          label: "Total Spend",
-          opts: [
-            "data-target": "campaign-form.totalBudget",
-            "data-action": "change->campaign-form#calculateImpressions",
-            step: "0.01"
-          ]
-        ],
-        impression_count: [
-          type: :number_input,
-          label: "Estimated Impressions",
-          opts: ["data-target": "campaign-form.estimatedImpressions"]
-        ],
-        us_hours_only: [
-          type: :checkbox,
-          label: "US Hours Only",
-          opts: [
-            hint: "If checked, this campaign will only run between 5AM - 5PM PST (San Francisco)"
-          ]
-        ],
-        weekdays_only: [
-          type: :checkbox,
-          label: "Weekdays Only",
-          opts: [
-            hint:
-              "If checked, this campaign will only run on weekdays in the user's local timezone"
-          ]
-        ],
-        start_date: [
-          type: :hidden_input,
-          label: "",
-          opts: ["data-target": "date-range.startDate"]
-        ],
-        end_date: [
-          type: :hidden_input,
-          label: "",
-          opts: ["data-target": "date-range.endDate"]
         ]
-      ]
+      ],
+      name: [type: :text_input, label: "Name", opts: [disabled: !is_admin]],
+      active_dates: [
+        type: :text_input,
+        label: "Date Range",
+        opts: [
+          disabled: !is_admin,
+          data: [target: "date-range.datePicker"]
+        ]
+      ],
+      status: [
+        type: :select,
+        label: "Status",
+        opts: [choices: CodeFund.Campaigns.statuses()]
+      ],
+      audience_id: [type: :select, label: "Audience", opts: [choices: audiences]],
+      creative_id: [
+        type: :select,
+        label: "Creative",
+        opts: [
+          prompt: "Select a Creative",
+          choices: creatives,
+          data: [target: "campaign-form.creatives"]
+        ]
+      ],
+      redirect_url: [type: :text_input, label: "Redirect URL", opts: [placeholder: "https://"]],
+      budget_daily_amount: [
+        type: :currency_input,
+        label: "Daily Max Spend",
+        opts: [step: "0.01"]
+      ],
+      total_spend: [
+        type: :currency_input,
+        label: "Total Spend",
+        opts: [
+          step: "0.01",
+          data: [
+            target: "campaign-form.totalBudget",
+            action: "change->campaign-form#calculateImpressions"
+          ]
+        ]
+      ],
+      ecpm: [
+        type: :currency_input,
+        label: "eCPM",
+        opts: [
+          step: "0.01",
+          min: "0",
+          step: "0.01",
+          data: [
+            target: "campaign-form.ecpm",
+            action: "change->campaign-form#calculateImpressions"
+          ]
+        ]
+      ],
+      included_countries: [
+        type: :multiple_select,
+        label: "Included Countries",
+        opts: [
+          data: [
+            target: "campaign-form.includedCountries",
+            action: "change->campaign-form#generateEstimates",
+            key: "included_countries"
+          ],
+          class: "form-control campaign-form selectize",
+          choices: Framework.Geolocation.Countries.list(),
+          hint: ~s"""
+          <div>
+            <span class="click-option" data-action='click->campaign-form#selectRecommendedCountries'>Recommended</span> |
+            <span class="click-option" data-action='click->campaign-form#selectPopularCountries'>Popular</span> |
+            <span class="click-option" data-action='click->campaign-form#selectAllCountries'>All</span> |
+            <span class="click-option" data-action='click->campaign-form#selectNoCountries'>None</span>
+          </div>
+          """
+        ]
+      ],
+      impression_count: [
+        type: :number_input,
+        label: "Estimated Impressions",
+        opts: [
+          disabled: true,
+          data: [target: "campaign-form.estimatedImpressions"]
+        ]
+      ],
+      us_hours_only: [
+        type: :checkbox,
+        label: "US Hours Only",
+        opts: [
+          hint: "If checked, this campaign will only run between 5AM - 5PM PST (San Francisco)"
+        ]
+      ],
+      weekdays_only: [
+        type: :checkbox,
+        label: "Weekdays Only",
+        opts: [
+          hint: "If checked, this campaign will only run on weekdays in the user's local timezone"
+        ]
+      ],
+      start_date: [
+        type: :hidden_input,
+        label: "",
+        opts: [
+          data: [target: "date-range.startDate"]
+        ]
+      ],
+      end_date: [
+        type: :hidden_input,
+        label: "",
+        opts: [
+          data: [target: "date-range.endDate"]
+        ]
+      ],
     ]
-  end
 
-  defp form_fields(false, user),
-    do: [fields: form_fields(true, user)[:fields] |> Keyword.drop([:audience_id, :total_spend])]
+    admin_only_fields = [
+      :audience_id,
+      :ecpm,
+      :impression_count,
+      :included_countries,
+      :total_spend,
+      :us_hours_only,
+      :user_id,
+      :weekdays_only,
+    ]
+
+    fields =
+      if is_admin do
+        fields
+      else
+        fields = Enum.reject(fields, fn({key, _}) -> Enum.member?(admin_only_fields, key) end)
+      end
+
+    [fields: fields]
+  end
 
   defp audiences_and_creatives_by_user(user) do
     [
