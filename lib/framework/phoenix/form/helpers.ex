7518861366cb25e61b06b4104d1465e943bbddf0
Nathan Hopkins <natehop@gmail.com>
2018-10-09 16:21:31 -0600

    Factor case statement to functions

--- lib/framework/phoenix/form/helpers.ex
+++ lib/framework/phoenix/form/helpers.ex
@@ -101,7 +101,11 @@ defmodule Framework.Phoenix.Form.Helpers do
     field_html =
       case type do
         :checkbox ->
-          checkbox(form, field_name, Keyword.put(opts, :class, "#{opts[:class]} form-check-input"))
+          checkbox(
+            form,
+            field_name,
+            Keyword.put(opts, :class, "#{opts[:class]} form-check-input")
+          )
 
         :select ->
           select_fields(
@@ -120,38 +124,57 @@ defmodule Framework.Phoenix.Form.Helpers do
           )
 
         :currency_input ->
-          addon_input(form, field_name, "$", Keyword.put(opts, :class, "#{opts[:class]} form-control"))
+          addon_input(
+            form,
+            field_name,
+            "$",
+            Keyword.put(opts, :class, "#{opts[:class]} form-control")
+          )
 
         :percentage_input ->
-          addon_input(form, field_name, "%", Keyword.put(opts, :class, "#{opts[:class]} form-control"))
+          addon_input(
+            form,
+            field_name,
+            "%",
+            Keyword.put(opts, :class, "#{opts[:class]} form-control")
+          )
 
         :image_preview ->
           image_preview(opts[:src])
 
         _other ->
-          apply(Phoenix.HTML.Form, type, [form, field_name, Keyword.put(opts, :class, "#{opts[:class]} form-control")])
+          apply(Phoenix.HTML.Form, type, [
+            form,
+            field_name,
+            Keyword.put(opts, :class, "#{opts[:class]} form-control")
+          ])
       end
 
-    case type do
-      :checkbox ->
-        field_html
-        |> checkbox_input_group(
-          label,
-          opts[:hint]
-        )
-
-      _ ->
-        field_html
-        |> input_group(
-          label,
-          hint_tag(opts[:hint]),
-          error_tag(form.source.assigns |> Map.get(:changeset), field_name)
-        )
-    end
+    render_input_group({field_name, field_html, [type: type, label: label, opts: opts]}, form)
   end
 
   defp render_field({field_name, args_list}, form) do
     render_field({field_name, Keyword.merge(args_list, opts: [])}, form)
+  end
+
+  defp render_input_group(
+         {field_name, field_html, [type: :checkbox, label: label, opts: opts]},
+         form
+       ) do
+    field_html
+    |> checkbox_input_group(
+      label,
+      opts[:hint]
+    )
+  end
+
+  defp render_input_group({field_name, field_html, [type: type, label: label, opts: opts]}, form) do
+    field_html
+    |> input_group(
+      label,
+      hint_tag(opts[:hint]),
+      error_tag(form.source.assigns |> Map.get(:changeset), field_name)
+    )
   end
 
   defp image_preview(nil), do: nil
