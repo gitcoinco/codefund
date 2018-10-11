e7af888cede7b9dbefbe50be96dee7e0b86e695c
Nathan Hopkins <natehop@gmail.com>
2018-10-11 11:53:18 -0600

    Add paid stats

---
+++ lib/code_fund/queries/user_impression.ex
@@ -0,0 +1,63 @@
+defmodule CodeFund.Query.UserImpression do
+  use CodeFundWeb, :query
+  alias CodeFund.Schema.UserImpression
+
+  def paid(query \\ UserImpression) do
+    from(user_impression in query,
+      where: user_impression.house_ad == false
+    )
+  end
+
+  def last_thirty_days(query \\ UserImpression) do
+    from(user_impression in query,
+      where:
+        fragment(
+          "?::date between ?::date and ?::date",
+          user_impression.inserted_at,
+          ^thirty_days_ago_as_date(),
+          ^now_as_date()
+        )
+    )
+  end
+
+  def impression_count_for_last_thirty_days(query \\ UserImpression) do
+    query = query |> last_thirty_days()
+
+    from(user_impression in query,
+      select: count(user_impression.id)
+    )
+  end
+
+  def paid_impression_count_for_last_thirty_days(query \\ UserImpression) do
+    paid(query) |> last_thirty_days()
+  end
+
+  def click_count_for_last_thirty_days(query \\ UserImpression) do
+    query = query |> last_thirty_days()
+
+    from(user_impression in query,
+      where: not is_nil(user_impression.redirected_at),
+      select: count(user_impression.id)
+    )
+  end
+
+  def paid_click_count_for_last_thirty_days(query \\ UserImpression) do
+    paid(query) |> click_count_for_last_thirty_days()
+  end
+
+  def distribution_amount_for_last_thirty_days(query \\ UserImpression) do
+    query = query |> paid() |> last_thirty_days()
+
+    from(user_impression in query,
+      select: sum(user_impression.distribution_amount)
+    )
+  end
+
+  defp thirty_days_ago_as_date() do
+    Timex.now() |> Timex.to_date() |> Timex.shift(days: -30) |> Timex.to_date()
+  end
+
+  defp now_as_date() do
+    Timex.now() |> Timex.to_date()
+  end
+end
