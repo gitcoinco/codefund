5a39ba82b6af8471e16938e0931b26e5275879ee
Nathan Hopkins <natehop@gmail.com>
2018-10-12 17:53:28 -0600

    Remove redundant doctests

--- test/code_fund/queries/impression_test.ex
+++
@@ -1,4 +0,0 @@
-defmodule CodeFund.Query.ImpressionTest do
-  use ExUnit.Case, async: true
-  doctest CodeFund.Query.Impression
-end
--- test/code_fund/queries/user_impression_test.ex
+++
@@ -1,4 +0,0 @@
-defmodule CodeFund.Query.UserImpressionTest do
-  use ExUnit.Case, async: true
-  doctest CodeFund.Query.UserImpression
-end
